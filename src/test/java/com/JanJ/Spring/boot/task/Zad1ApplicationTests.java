bfdce3c0826633f8dbda85cf5db7fc5b383b32ae
TwojeImie <janekjakjanek@gmail.com>
2025-01-20 19:53:37 +0100

    Initial commit

---
+++ src/test/java/com/JanJ/Spring/boot/task/Zad1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JanJ.Spring.boot.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zad1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
